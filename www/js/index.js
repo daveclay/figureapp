51557849931a40bfe8b044dbeb043982bada285b
Dave Clay <David.Clay@IGT.com>
2014-01-01 23:21:42 -0800

    Adding figure images

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,13 @@
  * under the License.
  */
 var app = {
+
+    getAppFilePath: function() {
+        var path = window.location.pathname;
+        path = path.substr( 0, path.length - 10 );
+        return 'file://' + path;
+    },
+
     // Application Constructor
     initialize: function() {
         this.bindEvents();
